27c667a8cf1efd503ea1c2b8987ebbaee452988e
Lukas Plank <lupl@tuta.io>
2025-08-25 14:45:36 +0200

    chore(tests): introduce testcontainers Fuseki fixture
    The change introduces a global testcontainers fixture that starts and exposes a Fuseki
    triplestore.
    
    Closes #5.

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+"""Global fixture definitions for the SPARQLx test suite."""
+
+import pytest
+
+from testcontainers.core.container import DockerContainer
+from testcontainers.core.waiting_utils import wait_for_logs
+
+
+@pytest.fixture(scope="session")
+def fuseki_service():
+    with DockerContainer("secoresearch/fuseki").with_exposed_ports(3030) as container:
+        wait_for_logs(container, "Start Fuseki")
+
+        host = container.get_container_host_ip()
+        port = container.get_exposed_port(3030)
+
+        sparql_endpoint = f"http://{host}:{port}/ds/sparql"
+
+        yield sparql_endpoint
