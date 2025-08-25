408967189b3c8bcf1eb6701ae48e60123b777d30
Lukas Plank <lupl@tuta.io>
2025-08-25 14:45:36 +0200

    chore(tests): introduce testcontainers Fuseki fixture
    The change introduces a global testcontainers fixture that starts and exposes a Fuseki
    triplestore.
    
    Closes #5.

--- tests/test_sparqlwrapper/test_sparqlwrapper_basic_query_operations.py
+++ tests/test_sparqlwrapper/test_sparqlwrapper_basic_query_operations.py
@@ -1,3 +1,21 @@
 """Pytest entry point for basic SPARQLWrapper Query Operation tests."""
 
 from sparqlx import SPARQLWrapper
+
+
+def test_sparqlwrapper_query(fuseki_service):
+    endpoint = fuseki_service
+    sparqlwrapper = SPARQLWrapper(endpoint=endpoint)
+
+    query = """
+    SELECT ?x ?y
+    WHERE {
+      VALUES (?x ?y) {
+        (1 2)
+        (3 4)
+      }
+    }
+    """
+
+    result = sparqlwrapper.query(query, to_rdflib=True)
+    assert list(result) == [{"x": 1, "y": 2}, {"x": 3, "y": 4}]
