621e33a1ae7ff28a3bd4774848b60fb50cd100f7
Lukas Plank <lupl@tuta.io>
2025-09-02 14:57:53 +0200

    test: implement SPARQLWrapper.query request parameters tests

---
+++ tests/test_sparqlwrapper/test_sparqlwrapper_query_request_params.py
@@ -0,0 +1,82 @@
+from typing import NamedTuple
+
+import pytest
+from sparqlx import SPARQLWrapper
+
+from utils import parse_reponse_qs
+
+
+class ProtocolRequestParameters(NamedTuple):
+    kwargs: dict
+    expected: dict
+
+
+params = [
+    ProtocolRequestParameters(
+        kwargs={"named_graph_uri": "https://named.graph"},
+        expected={
+            "query": ["select * where {?s ?p ?o}"],
+            "named-graph-uri": ["https://named.graph"],
+        },
+    ),
+    ProtocolRequestParameters(
+        kwargs={
+            "default_graph_uri": "https://default.graph",
+            "named_graph_uri": "https://named.graph",
+        },
+        expected={
+            "query": ["select * where {?s ?p ?o}"],
+            "default-graph-uri": ["https://default.graph"],
+            "named-graph-uri": ["https://named.graph"],
+        },
+    ),
+    ProtocolRequestParameters(
+        kwargs={
+            "default_graph_uri": "https://default.graph",
+            "named_graph_uri": "https://named.graph",
+            "version": "1.2",
+        },
+        expected={
+            "query": ["select * where {?s ?p ?o}"],
+            "default-graph-uri": ["https://default.graph"],
+            "named-graph-uri": ["https://named.graph"],
+            "version": ["1.2"],
+        },
+    ),
+    ProtocolRequestParameters(
+        kwargs={
+            "default_graph_uri": "https://default.graph",
+            "named_graph_uri": ["https://named.graph", "https://othernamed.graph"],
+        },
+        expected={
+            "query": ["select * where {?s ?p ?o}"],
+            "default-graph-uri": ["https://default.graph"],
+            "named-graph-uri": ["https://named.graph", "https://othernamed.graph"],
+        },
+    ),
+    ProtocolRequestParameters(
+        kwargs={
+            "default_graph_uri": [
+                "https://default.graph",
+                "https://otherdefault.graph",
+            ],
+            "named_graph_uri": ["https://named.graph", "https://othernamed.graph"],
+        },
+        expected={
+            "query": ["select * where {?s ?p ?o}"],
+            "default-graph-uri": [
+                "https://default.graph",
+                "https://otherdefault.graph",
+            ],
+            "named-graph-uri": ["https://named.graph", "https://othernamed.graph"],
+        },
+    ),
+]
+
+
+@pytest.mark.parametrize("param", params)
+def test_sparqlwrapper_query_request_params(param, fuseki_service):
+    sparqlwrapper = SPARQLWrapper(endpoint=fuseki_service.sparql_endpoint)
+    response = sparqlwrapper.query("select * where {?s ?p ?o}", **param.kwargs)
+
+    assert parse_reponse_qs(response) == param.expected
