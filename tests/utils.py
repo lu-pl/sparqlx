0abc7cc34cde2b7e4d3824100c670d1941c48f7e
Lukas Plank <lupl@tuta.io>
2025-10-28 15:06:25 +0100

    chore: fix typo

--- tests/utils.py
+++ tests/utils.py
@@ -7,7 +7,7 @@ from urllib.parse import parse_qs
 import httpx
 
 
-def parse_reponse_qs(response: httpx.Response) -> dict[str, list]:
+def parse_response_qs(response: httpx.Response) -> dict[str, list]:
     content = response.request.content.decode("utf-8")
     return parse_qs(content)
 
