a126852820b51f6ac202be8564f335f7ba1e64c4
Lukas Plank <lupl@tuta.io>
2025-12-17 08:24:37 +0100

    refactor: split converter dispatch and query parameter logic
    The change implements a refactor that splits the converter dispatch and
    query parameter logic.
    
    Converter dispatch is allocated to distinct functions `_get_query_type`
    and `_get_response_coverter` that live in `sparqlx.utils.utils`.
    
    The `QueryOperationParameters` class is moved to a separate module
    `sparqlx.utils.operation_parameters` and refactored such that its
    responsibility is just query parameter computation (and not converter
    dispatch as well).
    
    The change also `renames bindings_format_map`/`graph_format_map` to
    `sparql_result_response_format_map` and `rdf_response_format_map` to match
    the respective types `sparqlx.types.SPARQLResultResponseFormat` and
    `sparqlx.types.RDFResponseFormat` and ergo the SPARQL Protocol spec.

--- src/sparqlx/utils/utils.py
+++ src/sparqlx/utils/utils.py
@@ -1,122 +1,43 @@
-from collections import UserDict
+from typing import cast
 
 from rdflib.plugins.sparql import prepareQuery
 from rdflib.plugins.sparql.sparql import Query
-from sparqlx.types import RequestDataValue, SPARQLResponseFormat
+from sparqlx.types import SPARQLQuery, SPARQLQueryTypeLiteral, SPARQLResponseFormat
 from sparqlx.utils.converters import _convert_ask, _convert_bindings, _convert_graph
 
 
-class MimeTypeMap(UserDict):
-    def __missing__(self, key):
-        return key
+class SPARQLParseException(Exception): ...
 
 
-bindings_format_map = MimeTypeMap(
-    {
-        "json": "application/sparql-results+json",
-        "xml": "application/sparql-results+xml",
-        "csv": "text/csv",
-        "tsv": "text/tab-separated-values",
-    }
-)
-graph_format_map = MimeTypeMap(
-    {
-        "turtle": "text/turtle",
-        "xml": "application/rdf+xml",
-        "ntriples": "application/n-triples",
-        "json-ld": "application/ld+json",
-    }
-)
+def _get_query_type(query: SPARQLQuery) -> SPARQLQueryTypeLiteral:
+    try:
+        _prepared_query: Query = prepareQuery(query)
+    except Exception as exc:
+        raise SPARQLParseException(exc) from exc
+    else:
+        query_type = _prepared_query.algebra.name
+
+    return cast(SPARQLQueryTypeLiteral, query_type)
 
 
-class SPARQLOperationDataMap(UserDict):
-    def __init__(self, **kwargs):
-        self.data = {k.replace("_", "-"): v for k, v in kwargs.items() if v is not None}
+def _get_response_converter(
+    query_type: SPARQLQueryTypeLiteral, response_format: SPARQLResponseFormat | str
+):
+    if query_type in ["SelectQuery", "AskQuery"] and response_format not in [
+        "application/json",
+        "application/sparql-results+json",
+    ]:
+        msg = "JSON response format required for convert=True on SELECT and ASK query results."
+        raise ValueError(msg)
 
+    match query_type:
+        case "SelectQuery":
+            converter = _convert_bindings
+        case "AskQuery":
+            converter = _convert_ask
+        case "DescribeQuery" | "ConstructQuery":
+            converter = _convert_graph
+        case _:  # pragma: no cover
+            raise ValueError(f"Unsupported query type: {query_type}")
 
-class SPARQLParseException(Exception):
-    """Exception for signalling a SPARQL parsing failure."""
-
-
-class QueryOperationParameters:
-    def __init__(
-        self,
-        query: str,
-        convert: bool | None = None,
-        response_format: SPARQLResponseFormat | str | None = None,
-        version: str | None = None,
-        default_graph_uri: RequestDataValue = None,
-        named_graph_uri: RequestDataValue = None,
-    ) -> None:
-        self._query = query
-        self._convert = convert
-        self._response_format = response_format
-
-        try:
-            self._prepared_query: Query = prepareQuery(query)
-        except Exception as exc:
-            raise SPARQLParseException(exc) from exc
-        else:
-            self._query_type: str = self._prepared_query.algebra.name
-
-        self.data: SPARQLOperationDataMap = SPARQLOperationDataMap(
-            query=query,
-            version=version,
-            default_graph_uri=default_graph_uri,
-            named_graph_uri=named_graph_uri,
-        )
-        self.headers = {
-            "Accept": self.response_format,
-            "Content-Type": "application/x-www-form-urlencoded",
-        }
-
-    @property
-    def converter(self):
-        match self._query_type:
-            case "SelectQuery":
-                converter = _convert_bindings
-            case "AskQuery":
-                converter = _convert_ask
-            case "DescribeQuery" | "ConstructQuery":
-                converter = _convert_graph
-            case _:  # pragma: no cover
-                raise ValueError(f"Unsupported query type: {self._query_type}")
-
-        return converter
-
-    @property
-    def response_format(self) -> str:
-        match self._query_type:
-            case "SelectQuery" | "AskQuery":
-                _response_format = bindings_format_map[self._response_format or "json"]
-
-                if self._convert and _response_format not in [
-                    "application/json",
-                    "application/sparql-results+json",
-                ]:
-                    msg = "JSON response format required for convert=True on SELECT and ASK query results."
-                    raise ValueError(msg)
-
-            case "DescribeQuery" | "ConstructQuery":
-                _response_format = graph_format_map[self._response_format or "turtle"]
-            case _:  # pragma: no cover
-                raise ValueError(f"Unsupported query type: {self._query_type}")
-
-        return _response_format
-
-
-class UpdateOperationParameters:
-    def __init__(
-        self,
-        update_request: str,
-        version: str | None = None,
-        using_graph_uri: RequestDataValue = None,
-        using_named_graph_uri: RequestDataValue = None,
-    ):
-        self.data: SPARQLOperationDataMap = SPARQLOperationDataMap(
-            update=update_request,
-            version=version,
-            using_graph_uri=using_graph_uri,
-            using_named_graph_uri=using_named_graph_uri,
-        )
-        self.headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    return converter
